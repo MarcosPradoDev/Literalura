5f7130d2f446b116fc92ad834fed8f4a9435b7c9
Marcos Prado <marcos_prado_tec@hotmail.com>
2024-07-11 22:20:53 -0300

    Primeiro Commit

--- src/main/java/br/com/desafio/Literalura/principal/Principal.java
+++ src/main/java/br/com/desafio/Literalura/principal/Principal.java
@@ -2,7 +2,7 @@ package br.com.desafio.Literalura.principal;
 
 public class Principal {
     public void exibeMenu() {
-        System.out.println("Commit teste");
+        System.out.println("Commit teste1");
 
     }
 }
